77083bac9725bda37fbc279adabc3b9e04fa6ad1
tritbool <deadubed@gmail.com>
2017-01-19 17:13:24 +0100

    Updated and commented Main class

--- videogenTest/src/videogenTest/Main.java
+++ videogenTest/src/videogenTest/Main.java
@@ -7,17 +7,19 @@ public class Main {
 	private static playlistTools pst=new playlistTools();
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//pst.test1();
-		//pst.genVignette("Videos/V6.mp4");
-		//pst.genDuration("output.mp4");
-		//pst.playlistToVignettes(pst.videogenToPlaylistFull("foo2bis.videogen"));
-		//pst.videoGenToHTML("foo2bis.videogen");
-		//pst.videogenGenerator("foo2bis.videogen");
-		pst.genDuration("Videos/V6.mp4");
+		//Generates all the pictures for the videos described in the given file
+		pst.playlistToVignettes(pst.videogenToPlaylistFull("foo2bis.videogen"));
+		//generates an HTML file given the videogen model
+		pst.videoGenToHTML("foo2bis.videogen");
+		//Concatenates files given in out.ffmpeg
 		pst.concatFFMPEG();
+		//Generates an extended M3U file
 		pst.playlistToM3UExtended(pst.videogenToPlaylistFull("foo2bis.videogen"));
-		//pst.videogenToFml("foo2bis.videogen");
+		//Using the .jar app (in GUI mode) of FAMILIAR, we see that this feature model has 48 possible configurations The generated script is runnable in the FAMILIAR tool V1.1
+		pst.videogenToFml("foo2bis.videogen");
+		//Generates a videogen model from a given folder
+		pst.videoGenFromFolder("Videos");
+		
 	}
 
 }
