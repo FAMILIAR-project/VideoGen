61c2745560cbcca0a7268f3e4b2844828062f392
nanou <annaig.butterworth@etudiant.univ-rennes1.fr>
2017-01-22 23:33:33 +0100

    Add a transformation that reads a videogen model and generates videos

---
+++ videogen/src/videogen/RandomVideoGeneratorSwitch.java
@@ -0,0 +1,86 @@
+package videogen;
+
+import java.io.File;
+
+import org.eclipse.emf.common.util.EList;
+import org.xtext.example.mydsl.videoGen.AlternativeVideoSeq;
+import org.xtext.example.mydsl.videoGen.MandatoryVideoSeq;
+import org.xtext.example.mydsl.videoGen.OptionalVideoSeq;
+import org.xtext.example.mydsl.videoGen.VideoDescription;
+import org.xtext.example.mydsl.videoGen.VideoGeneratorModel;
+import org.xtext.example.mydsl.videoGen.VideoSeq;
+import org.xtext.example.mydsl.videoGen.util.VideoGenSwitch;
+
+/**
+ * Warning : as of now, this generator only works for AVI specified video files
+ *
+ * @author annaig
+ *
+ */
+public class RandomVideoGeneratorSwitch extends VideoGenSwitch<Boolean> {
+
+	@Override
+	public Boolean caseVideoGeneratorModel(VideoGeneratorModel videoGenModel) {
+		EList<VideoSeq> videoseqs = videoGenModel.getVideoseqs();
+		for (VideoSeq videoseq : videoseqs) {
+			doSwitch(videoseq);
+		}
+		return true;
+
+	}
+
+	@Override
+	public Boolean caseVideoDescription(VideoDescription videoDesc) {
+		String location = videoDesc.getLocation();
+		generateRandomVideo(location);
+		return true;
+	}
+
+	private void generateRandomVideo(String location) {
+		File file = new File(location);
+
+		File parentFile = file.getParentFile();
+		String parentPath = ".";
+		if (parentFile != null) {
+			parentPath = parentFile.getPath();
+		}
+
+		// if (file.exists()) {
+		// try {
+		// FileUtils.forceDelete(file);
+		// } catch (IOException e) {
+		// throw new RuntimeException(e);
+		// }
+		// }
+
+		new File(parentPath).mkdirs();
+		int duration = Utils.genRandomInt(10);
+		System.out.println(String.format("Generating [%s] with duration = %d", location, duration));
+		FFMPEGHelper.randomGenerationVideo(location, duration);
+		System.out.println(" --> done");
+	}
+
+	@Override
+	public Boolean caseMandatoryVideoSeq(MandatoryVideoSeq mandatoryVideoseq) {
+		VideoDescription description = mandatoryVideoseq.getDescription();
+		doSwitch(description);
+		return true;
+	}
+
+	@Override
+
+	public Boolean caseOptionalVideoSeq(OptionalVideoSeq optionalVideoseq) {
+		VideoDescription description = optionalVideoseq.getDescription();
+		doSwitch(description);
+		return true;
+	}
+
+	@Override
+	public Boolean caseAlternativeVideoSeq(AlternativeVideoSeq alternativeVideoseq) {
+		EList<VideoDescription> videodescs = alternativeVideoseq.getVideodescs();
+		for (VideoDescription desc : videodescs) {
+			doSwitch(desc);
+		}
+		return true;
+	}
+}
